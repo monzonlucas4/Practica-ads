ed4a110ece21fd719337f19e7a65ce4f08b6d43f
monzonlucas <monzon.lucas4@gmail.com>
2019-03-13 09:38:47 -0300

    Merge branch 'CaracteristicasGino' of https://github.com/monzonlucas4/Practica-ads into BranchMonzon
    # Conflicts:
    #	Codigo.java

--- Codigo.java
+++ Codigo.java
@@ -3,7 +3,12 @@ public class prueba {
 
 	public String metodo(){
 
+<<<<<<< HEAD
 		return "Practica ads - Lucas";
+=======
+		return "Practica agrego Gino ahora";
+
+>>>>>>> 0fed8cbfc790d1411b857936274a3eef8f38891c
 	}
 
 	//Cambiando le codigo en el branchMonzón
